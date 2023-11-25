e0478ddea572da8a8e8d59e81b7990ebd822d9d5
motikariscadyana <motikariscadyana@gmail.com>
2023-11-25 19:06:22 +0700

    tugas

--- App.tsx
+++ App.tsx
@@ -1,118 +1,49 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, Text, ScrollView, Image, View } from 'react-native';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+export default function App() {
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+    const [data, setData] = useState()
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+    const getData = async () => {
+        try {
+            const res = await axios.get('https://newsapi.org/v2/top-headlines', {
+                params: {
+                    country: 'US',
+                    category: 'business',
+                    apiKey: '9c17592f711e42f28e3bbb6311a284d1',
+                },
+            });
+            setData(res.data.articles)
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+        } catch (error) {
+            alert(error.massage);
+        }
+    };
+
+    useEffect(() => {
+        getData()
+    }, [])
+
+    return (
+        <>
+            {/*console.log(data)*/}
+            <SafeAreaView>
+                <ScrollView>
+                    {data && data.map((item, i) => {
+                        return <>
+                            <View style={{ flexDirection: 'row', marginVertical: 10, marginHorizontal: 5 }}>
+                                <image style={{ width: 100, height: 100 }} source={{ uri: item.urlToImage }} />
+                                <View style={{ justifyContent: 'space-between' }}>
+                                    <text style={{ fontWeight: 'bold' }}>{item.title}</text>
+                                    <text>{item.author}</text>
+                                </View>
+                            </View>
+                        </>;
+                    })}
+                </ScrollView>
+            </SafeAreaView>
+        </>
+    )
 }
-
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
